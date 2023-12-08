12b341df03da9356f3963b1d51d7e27bb8d923d2
CIC4DA <dhruv.jain9100@gmail.com>
2023-12-08 18:15:49 +0530

    setting up ingress-nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,11 @@ import {json} from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentUser', (req,res) => {
+    res.send("RESPODSFOIHDSOFIHDSOFHI ");
+})
+
 app.listen(3000, ()=> {
     console.log("Listining on port 3000!");
-    console.log("New console log");
+    console.log("New console log 2");
 })
