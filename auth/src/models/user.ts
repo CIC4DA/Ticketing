587217ea46da47ddfa227c0cc2a5dce65266b58f
CIC4DA <dhruv.jain9100@gmail.com>
2023-12-10 12:57:03 +0530

    new schema pattern specially for typescript

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,18 @@
 import mongoose, { mongo } from "mongoose";
 
+// An interface that describes the properties
+// that are required to make a New User
+interface UserAttributes {
+    email: string,
+    password: string
+}
+
+// An interface that describes the properties
+// that an user model has
+interface UserModel extends mongoose.Model<any> {
+    build(attributes: UserAttributes): any;
+}
+
 const userSchema = new mongoose.Schema({
     email : {
         type : String,
@@ -10,6 +23,10 @@ const userSchema = new mongoose.Schema({
         required : true
     }
 });
+// we will call this function to make use of typescript interface
+userSchema.statics.build = (attributes : UserAttributes) => {
+    return new User(attributes);
+};
 
-const User = mongoose.model('User',userSchema);
-export {User};
+const User = mongoose.model<any, UserModel>('User',userSchema);
+export {User };
