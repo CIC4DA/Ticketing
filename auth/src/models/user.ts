5b6bae7bd22e4c0a762adee800a772868494f7a4
CIC4DA <dhruv.jain9100@gmail.com>
2023-12-10 13:04:37 +0530

    perfected the User Schema (important)

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -8,10 +8,19 @@ interface UserAttributes {
 }
 
 // An interface that describes the properties
-// that an user model has
-interface UserModel extends mongoose.Model<any> {
-    build(attributes: UserAttributes): any;
+// that an User Document has
+interface UserDoc extends mongoose.Document {
+    email: string,
+    password: string
 }
+
+// An interface that describes the properties
+// that an user model has
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attributes: UserAttributes): UserDoc;
+}
+
+
 
 const userSchema = new mongoose.Schema({
     email : {
@@ -23,10 +32,18 @@ const userSchema = new mongoose.Schema({
         required : true
     }
 });
+
 // we will call this function to make use of typescript interface
 userSchema.statics.build = (attributes : UserAttributes) => {
     return new User(attributes);
 };
 
-const User = mongoose.model<any, UserModel>('User',userSchema);
-export {User };
+// now we can use User.build() to build a new user;
+
+const User = mongoose.model<UserDoc, UserModel>('User',userSchema);
+export { User };
+
+
+// the angle bracket syntax is a generic syntax in typescript
+// this gives the all the types we are provinding the function
+// Second argument is the type of which the value will be returned
